921ae43b002c8a9fa36d2d461934729d2efa9193
shafayk <user7@sunrichtech.com>
2025-08-18 17:06:33 +0530

    SMART_NEWS_CHANGELIST
    1. added deserializer.setUseTypeMapperForKey(true); to resolved type mismatch exception while deserializing message.
    
    Author - Khan Shafay

--- src/main/java/com/example/kafka_workspace/smart_news/consumer/config/ConsumerConfigs.java
+++ src/main/java/com/example/kafka_workspace/smart_news/consumer/config/ConsumerConfigs.java
@@ -18,13 +18,15 @@ public class ConsumerConfigs {
   @Bean
   public ConsumerFactory<String, Article> consumerFactory() {
     JsonDeserializer<Article> deserializer = new JsonDeserializer<>(Article.class);
-    deserializer.addTrustedPackages("*");
+    deserializer.addTrustedPackages("com.example.kafka_workspace.smart_news.consumer.model");
+    deserializer.setUseTypeMapperForKey(true);
 
     Map<String, Object> consumerConfigs = Map.of(
         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
-        ConsumerConfig.GROUP_ID_CONFIG, "smart_news_group",
+        ConsumerConfig.GROUP_ID_CONFIG, "default_smart_news_group",
         ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
+        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"
     );
 
     return new DefaultKafkaConsumerFactory<>(consumerConfigs, new StringDeserializer(), deserializer);
